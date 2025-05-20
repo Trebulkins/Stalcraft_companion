ebb935486845801f4ade5502317ca47f73640b9b
Trebulkins <trebinivan2005@gmail.com>
2025-05-20 11:00:51 +0700

    v 0.0.5

--- app/src/main/java/com/example/stalcraft_companion/adapters/ItemDBAdapter.kt
+++ app/src/main/java/com/example/stalcraft_companion/adapters/ItemDBAdapter.kt
@@ -9,42 +9,45 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.stalcraft_companion.MainActivity
 import com.example.stalcraft_companion.R
-import com.example.stalcraft_companion.api.schemas.ListingItem
+import com.example.stalcraft_companion.api.schemas.Item
 import com.squareup.picasso.Picasso
 
-class ItemDBAdapter(var itemsList: List<ListingItem>, var context: Context, var listener: MainActivity.RecyclerItemListener) : RecyclerView.Adapter<ItemDBAdapter.SearchItemsHolder>() {
+class ItemDBAdapter(var itemsList: List<Item>, var context: Context, var listener: MainActivity.RecyclerItemListener) : RecyclerView.Adapter<ItemDBAdapter.SearchItemsHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchItemsHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.item_layout, parent, false)
 
-        val viewHolder = SearchItemsHolder(view)
+        val viewHolder = ItemDBHolder(view)
         view.setOnClickListener { v -> listener.onItemClick(v, viewHolder.adapterPosition) }
         return viewHolder
     }
 
-    override fun onBindViewHolder(holder: SearchItemsHolder, position: Int) {
-        holder.titleTextView.text = itemList[position].nameRu
-        holder.originalTitleTextView.text = itemList[position].nameOriginal
-        holder.releaseDateTextView.text = itemList[position].year.toString()
+    override fun onBindViewHolder(holder: ItemDBHolder, position: Int) {
+        holder.titleTextView.text = itemsList[position].nameRu
+        holder.originalTitleTextView.text = itemsList[position].nameOriginal
+        holder.releaseDateTextView.text = itemsList[position].year.toString()
 
         if (itemList[position].posterUrl != null) {
-            Picasso.get().load(RetrofitClient.TMDB_BASE_URL + itemList[position].posterUrl).into(holder.itemImageView)
+            Picasso.get().load(RetrofitClient.GITHUB_URL + itemList[position].posterUrl).into(holder.itemImageView)
         }
     }
 
     override fun getItemCount(): Int {
-        return itemList.size
+        return itemsList.size
     }
 
-    fun getItemAtPosition(pos: Int): Int {
-        return itemList[pos]
+    fun getItemAtPosition(pos: Int): Item {
+        return itemsList[pos]
     }
 
-    inner class SearchItemsHolder(v: View) : RecyclerView.ViewHolder(v) {
-        var titleTextView: TextView = v.findViewById(R.id.title_textview)
-        var originalTitleTextView: TextView = v.findViewById(R.id.original_title_textview)
-        var releaseDateTextView: TextView = v.findViewById(R.id.release_date_textview)
-        var itemImageView: ImageView = v.findViewById(R.id.item_imageview)
+    inner class ItemDBHolder(v: View) : RecyclerView.ViewHolder(v) {
+        var itemTitle: TextView = v.findViewById(R.id.item_title)
+        var itemCategory: TextView = v.findViewById(R.id.item_category)
+        var itemIcon: ImageView = v.findViewById(R.id.item_icon)
+        var itemRarity: View = v.findViewById(R.id.item_rarity_color)
+        var itemState: TextView = v.findViewById(R.id.item_state)
+        var itemStateImg: ImageView = v.findViewById(R.id.item_state_img)
+        var itemWeight: TextView = v.findViewById(R.id.item_weight)
 
         init {
             v.setOnClickListener { v: View ->
