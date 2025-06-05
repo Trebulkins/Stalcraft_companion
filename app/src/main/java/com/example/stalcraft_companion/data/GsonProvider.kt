ab3dd23a199452f93fb96a602a6a569749c19536
Trebulkins <trebinivan2005@gmail.com>
2025-06-05 23:39:12 +0700

    v 0.1.15

--- app/src/main/java/com/example/stalcraft_companion/data/GsonProvider.kt
+++ app/src/main/java/com/example/stalcraft_companion/data/GsonProvider.kt
@@ -8,22 +8,8 @@ import com.google.gson.GsonBuilder
 object GsonProvider {
     private fun getGsonBuilder(): GsonBuilder {
         return GsonBuilder()
-            .registerTypeAdapterFactory(
-                RuntimeTypeAdapterFactory.of(TranslationString::class.java, "type")
-                    .registerSubtype(TranslationString.Text::class.java, "text")
-                    .registerSubtype(TranslationString.Translation::class.java, "translation")
-            )
-            .registerTypeAdapterFactory(
-                RuntimeTypeAdapterFactory.of(InfoBlock::class.java, "type")
-                    .registerSubtype(InfoBlock.TextBlock::class.java, "text")
-                    .registerSubtype(InfoBlock.DamageBlock::class.java, "damage")
-                    .registerSubtype(InfoBlock.NumericBlock::class.java, "numeric")
-                    .registerSubtype(InfoBlock.ListBlock::class.java, "list")
-                    .registerSubtype(InfoBlock.KeyValueBlock::class.java, "key-value")
-                    .registerSubtype(InfoBlock.RangeBlock::class.java, "range")
-                    .registerSubtype(InfoBlock.UsageBlock::class.java, "usage")
-                    .registerSubtype(InfoBlock.ItemBlock::class.java, "item")
-            )
+            .registerTypeAdapter(InfoBlock::class.java, InfoBlockDeserializer())
+            .registerTypeAdapter(TranslationString::class.java, TranslationStringDeserializer())
     }
     val instance: Gson by lazy { getGsonBuilder().create() }
 }
---
+++ app/src/main/java/com/example/stalcraft_companion/data/InfoBlockDeserializer.kt
@@ -0,0 +1,80 @@
+package com.example.stalcraft_companion.data
+
+import com.example.stalcraft_companion.data.modles.InfoBlock
+import com.example.stalcraft_companion.data.modles.TranslationLines
+import com.example.stalcraft_companion.data.modles.TranslationString
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import com.google.gson.JsonParseException
+import com.google.gson.reflect.TypeToken
+import java.lang.reflect.Type
+
+class InfoBlockDeserializer : JsonDeserializer<InfoBlock> {
+    override fun deserialize(
+        json: JsonElement,
+        typeOfT: Type,
+        context: JsonDeserializationContext
+    ): InfoBlock {
+        val jsonObject = json.asJsonObject
+        return when (jsonObject.get("type").asString) {
+            "text" -> InfoBlock.TextBlock(
+                title = context.deserialize(jsonObject.get("title"), TranslationString::class.java),
+                text = context.deserialize(jsonObject.get("text"), TranslationString::class.java)
+            )
+            "damage" -> InfoBlock.DamageBlock(
+                startDamage = jsonObject.get("startDamage").asFloat,
+                damageDecreaseStart = jsonObject.get("damageDecreaseStart").asFloat,
+                endDamage = jsonObject.get("endDamage").asFloat,
+                damageDecreaseEnd = jsonObject.get("damageDecreaseEnd").asFloat,
+                maxDistance = jsonObject.get("maxDistance").asFloat
+            )
+            "range" -> InfoBlock.RangeBlock(
+                name = context.deserialize(jsonObject.get("name"), TranslationString::class.java),
+                min = jsonObject.get("min").asFloat,
+                max = jsonObject.get("max").asFloat
+            )
+            "list" -> InfoBlock.ListBlock(
+                title = context.deserialize(jsonObject.get("title"), TranslationString::class.java),
+                elements = context.deserialize(jsonObject.get("elements"), object : TypeToken<List<InfoBlock>>() {}.type)
+            )
+            "numeric" -> InfoBlock.NumericBlock(
+                name = context.deserialize(jsonObject.get("name"), TranslationString::class.java),
+                value = jsonObject.get("value").asFloat,
+                formatted = context.deserialize(jsonObject.get("formatted"), TranslationString::class.java)
+            )
+            "key-value" -> InfoBlock.KeyValueBlock(
+                key = context.deserialize(jsonObject.get("key"), TranslationString::class.java),
+                value = context.deserialize(jsonObject.get("value"), TranslationString::class.java)
+            )
+            "usage" -> InfoBlock.UsageBlock(
+                name = context.deserialize(jsonObject.get("name"), TranslationString::class.java)
+            )
+            "item" -> InfoBlock.ItemBlock(
+                name = context.deserialize(jsonObject.get("name"), TranslationString::class.java)
+            )
+            else -> throw JsonParseException("Unknown type")
+        }
+    }
+}
+
+class TranslationStringDeserializer: JsonDeserializer<TranslationString> {
+    override fun deserialize(
+        json: JsonElement,
+        typeOfT: Type,
+        context: JsonDeserializationContext
+    ): TranslationString {
+        val jsonObject = json.asJsonObject
+        return when (jsonObject.get("type").asString) {
+            "text" -> TranslationString.Text(
+                text = jsonObject.get("text").asString
+            )
+
+            "translation" -> TranslationString.Translation(
+                lines = context.deserialize(jsonObject.get("lines"), TranslationLines::class.java)
+            )
+
+            else -> throw JsonParseException("Unknown type")
+        }
+    }
+}
